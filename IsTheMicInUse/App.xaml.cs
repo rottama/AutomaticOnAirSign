4c88232824692cec0b28c440460c28cef6cc7fa7
rottama <77901727+rottama@users.noreply.github.com>
2021-02-08 16:14:11 +0100

    Update App.xaml.cs
    included startup check

--- IsTheMicInUse/App.xaml.cs
+++ IsTheMicInUse/App.xaml.cs
@@ -11,16 +11,22 @@ namespace IsTheMicInUse
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            if (_onlyOneAppInstanceIsRunningMutex.WaitOne(TimeSpan.Zero))
+            if (ConfigurationManager.AppSettings["TasmotaHostname"] == null)
             {
-                Poller poller = new Poller();
-                new Thread(poller.PollMicrophone).Start();
+                MessageBox.Show("Missing configuration file.", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Information);
+                Shutdown();
+                return;
             }
-            else
+
+            if (!_onlyOneAppInstanceIsRunningMutex.WaitOne(TimeSpan.Zero))
             {
                 MessageBox.Show("Already running.", Assembly.GetExecutingAssembly().GetName().Name, MessageBoxButton.OK, MessageBoxImage.Information);
                 Shutdown();
+                return;
             }
+
+            Poller poller = new Poller();
+            new Thread(poller.PollMicrophone).Start();
         }
     }
 }
